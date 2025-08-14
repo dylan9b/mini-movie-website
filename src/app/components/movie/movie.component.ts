5bfb777bc893171e90c399ffb4b3c3fed702ed13
dylan9b <dylan9b@gmail.com>
2025-08-14 21:37:01 +0200

    Added default styling to images which failed to load

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -17,10 +17,12 @@ import { MovieResponse } from '@pages/movies/_model/movie-response.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MovieComponent {
+  protected isImageLoaded = false;
+  protected isImageBroken = false;
   readonly movie = input.required<MovieResponse>();
 
   readonly movieImageSignal = computed(() => {
-    return this.movie()?.image?.url ?? 'assets/images/empty.jpg';
+    return this.movie()?.image?.url;
   });
 
   onPlayForFun(): void {
